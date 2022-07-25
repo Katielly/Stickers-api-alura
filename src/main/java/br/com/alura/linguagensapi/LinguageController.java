0ea0859a245e431c0002d6a601b6e3120aed5d08
Katielly <dev.katielly@gmail.com>
2022-07-25 09:20:04 -0400

    api-completa

---
+++ src/main/java/br/com/alura/linguagensapi/LinguageController.java
@@ -0,0 +1,33 @@
+package br.com.alura.linguagensapi;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+public class LinguageController {
+
+    @Autowired
+    private  LinguagemRepository repositorio;
+
+    @GetMapping("/linguagens")
+    public List<Linguagem> obterLinguagens() {
+        List<Linguagem> linguagens = repositorio.findAll();
+        return linguagens;
+    }
+
+    @PostMapping("/linguagens")
+    public Linguagem cadastrarLinguagens(@RequestBody Linguagem linguagem) {
+        Linguagem linguagemSalva =  repositorio.save(linguagem);
+        return linguagemSalva;
+    }
+        
+}
+    
+    
+
